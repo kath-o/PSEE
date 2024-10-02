2630c022405e0a4a7934cd609774eca01a21ed99
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-02 12:47:41 +0100

    setting a list of unique species names

--- GBIF.R
+++ GBIF.R
@@ -8,3 +8,11 @@ setwd("~/Desktop/MSc EEB/WD/PSEE/data")
 
 #loading data into a variable named dat.messy 
 dat.messy <- read.csv("PaciFlora/Species_list_full_2905.csv",sep=";")
+
+#this data frame contains data on species presence in Pacific islands
+#the same species will then appear multiple times
+#we will start by setting a list of unique species names
+spp <- unique(dat.messy$species)
+spp <- spp[-which(is.na(spp))] ##remove na data
+spp <- spp[1:200] ##we will only use the 200 first species to be quicker
+n_spp <- length(spp) ##the number of species. Here it is obviously 200, but in practice you may not know.
