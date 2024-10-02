1bcf7700a08c71d321e01428d1674d3bb5bff400
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-02 12:45:53 +0100

    loading data into variable

--- GBIF.R
+++ GBIF.R
@@ -6,3 +6,5 @@ library(rgbif)
 #downloaded PaciFlora data 
 setwd("~/Desktop/MSc EEB/WD/PSEE/data")
 
+#loading data into a variable named dat.messy 
+dat.messy <- read.csv("PaciFlora/Species_list_full_2905.csv",sep=";")
