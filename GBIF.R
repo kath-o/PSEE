e8805fb8eb1146751d1b1e664093f6de8ceac8b2
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-02 13:31:38 +0100

    for loop

--- GBIF.R
+++ GBIF.R
@@ -25,4 +25,38 @@ spp.check.ok <- spp.check.ok[-1,] ##we remove this row
 spp.check.bad <- name_backbone("xxx",verbose=T,strict=T) ##we initialize a data frame for a word that is not a species name.
 spp.check.bad <- spp.check.bad[-1,] ##we remove this row
 
+#for all unique species names, we will check them against the GBIF backbone taxonomy, and keep the accepted species name if possible
+##prepare progress bar – this is just to see how far we are in the loop. This is useful for large databases
+pb <- txtProgressBar(min=0, max=n_spp, initial=0,style = 3)
+
+for(i in 1:n_spp){
+  toto <- name_backbone(spp[i],verbose=T,strict=T) 
+  if(length(which(names(toto)=="acceptedUsageKey"))==1){ 
+    toto <- toto[,-which(names(toto)=="acceptedUsageKey")]
+  }
+  if(ncol(toto)==ncol(spp.check.ok)){
+    if(length(which(toto$status=="ACCEPTED"))>0){ 
+      spp.check.ok <- rbind(spp.check.ok,toto[which(toto$status=="ACCEPTED"),]) 
+    }else if(length(which(toto$status=="SYNONYM"))>0){ 
+      warning(paste("Species",spp[i],"is a synonmy")) 
+      spp.check.ok <- rbind(spp.check.ok,toto[which(toto$status=="SYNONYM")[1],]) 
+    }else if(length(which(toto$status=="DOUBTFUL"))>0){ 
+      warning(paste("Species",spp[i],"is doubtful"))
+      spp.check.ok <- rbind(spp.check.ok,toto[which(toto$status=="DOUBTFUL")[1],]) 
+    }else{
+      stop("Status unknown") 
+    }   
+  }else if(ncol(toto)==ncol(spp.check.bad)){
+    spp.check.bad <- rbind(spp.check.bad,toto)
+  }
+  else{
+    stop("Unknown length") 
+  }
+  info <- sprintf("%d%% done", round((i/n_spp)*100))
+  setTxtProgressBar(pb, i, label=info)
+}
+close(pb)
+
+#now want to examine our output
+#we will check if we have the same species appearing more than once: that would indicate that synonym were used in the database
 
